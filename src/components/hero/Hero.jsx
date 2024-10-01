32162b3d811aa1686d0fad24e38ddb5fb0871cbe
ibr <ibr.seckin@gmail.com>
2024-10-01 12:10:53 +0300

    up

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,14 +1,14 @@
 
 const Hero = () => {
   return (
-    <div className="flex flex-col md:flex-row items-start  justify-between gap-1 py-1 px-2 poppins-font ">
+    <div className="flex flex-col-reverse md:flex-row items-center md:items-start  md:justify-between gap-1 py-1 px-2 poppins-font ">
         <div className="  pt-20 flex-1">
-            <h1 className="text-6xl font-bold">WONDERFULL STORIES</h1>
-            <p className="text-gray-500 font-semibold mt-5">Listen to the little stories to improve your Russian and immerse yourself in the nuances of the language, enhancing your comprehension and fluency over time.</p>
+            <h1 className="text-3xl md:text-6xl font-bold text-center md:text-left ">WONDERFULL STORIES</h1>
+            <p className="text-sm md:text-md text-gray-500 font-semibold mt-5 text-center md:text-left">Listen to the little stories to improve your Russian and immerse yourself in the nuances of the language, enhancing your comprehension and fluency over time.</p>
 
         </div>
         <div className="flex-1">
-            <img src="/images/hero.png" alt="hero" className=" max-h-[500px]" />
+            <img src="/images/hero.png" alt="hero" className=" max-h-[300px] md:max-h-[500px]" />
         </div>
 
 
