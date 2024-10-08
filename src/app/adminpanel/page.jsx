f9208904c9d0a17d18e3fb11f2004f4f91867d63
ibr <ibr.seckin@gmail.com>
2024-10-08 14:57:28 +0300

    up

--- src/app/adminpanel/page.jsx
+++ src/app/adminpanel/page.jsx
@@ -1,21 +1,11 @@
 "use client";
 
-import Banner from "@/components/banner/Banner";
-import MainBlogCard from "./components/MainBlogCard";
-import RecentBlogCard from "./components/RecentBlogCard";
-import WithBgBlogCard from "./components/WithBgBlogCard";
-import { Provider } from "react-redux";
-import { persistor, store } from "@/lib/store";
-import PersistReducer from "redux-persist/lib/persistReducer";
-
+import Banner from "@/components/banner/Banner"; 
 const Adminpanel = () => {
   return (
     <div>
       <Banner path="/images/blog.jpg" title="Admin Panel" />
-
-      <Provider store={store}>
-        <PersistReducer loading={null} persistor={persistor}></PersistReducer>
-      </Provider>
+ 
     </div>
   );
 };
