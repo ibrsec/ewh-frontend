3b75f3d0089b9de81cc4c0ff8eee2d8956e35fb4
ibr <ibr.seckin@gmail.com>
2024-11-01 11:34:12 +0300

    format number fix

--- src/helpers/formatPhoneNumber.js
+++ src/helpers/formatPhoneNumber.js
@@ -4,6 +4,6 @@ const  formatPhoneNumber = (number) => {
 
     // Son 10 haneyi al ve formatla
     const lastTen = digits.slice(-10);
-    return `${number.slice(0,number.indexOf("0"))} (${lastTen.slice(0, 3)}) ${lastTen.slice(3, 6)} ${lastTen.slice(6, 8)} ${lastTen.slice(8)}`;
+    return `${number.slice(0,number.indexOf("0")+1)} (${lastTen.slice(0, 3)}) ${lastTen.slice(3, 6)} ${lastTen.slice(6, 8)} ${lastTen.slice(8)}`;
 }
 export default formatPhoneNumber;
